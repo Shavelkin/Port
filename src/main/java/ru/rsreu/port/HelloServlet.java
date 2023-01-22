72cc7dbb6e19f11f7a332723df396d7bc043cfc7
MishGunios <mishashasha.den@gmail.com>
2023-01-22 23:40:10 +0300

    port

--- src/main/java/ru/rsreu/port/HelloServlet.java
+++ src/main/java/ru/rsreu/port/HelloServlet.java
@@ -9,7 +9,7 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-        message = "Hello World!";
+        message = "Hello World! Prutzkow pidarasina suka";
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
