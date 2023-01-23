f40fd8c4e3a2aa23e34ec9f0e698fa82a7aa92f5
Shavelkin <shavelkinartem@outlook.com>
2023-01-23 12:59:07 +0300

    port

--- src/main/java/ru/rsreu/port/entity/User.java
+++ src/main/java/ru/rsreu/port/entity/User.java
@@ -1,11 +1,16 @@
 package ru.rsreu.port.entity;
 
 import ru.rsreu.port.entity.enums.Roles;
+import java.security.Principal;
 
 public class User {
-    private int id;
+    private Integer userId;
     private String name;
     private String login;
     private String password;
     private Roles userRole;
+
+    public Integer getUserId() {
+        return this.userId;
+    }
 }
