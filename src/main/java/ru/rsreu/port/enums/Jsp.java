8d411b81a3202039483cb54b231f8fda82f04417
MishGunios <mishashasha.den@gmail.com>
2023-02-05 19:51:58 +0300

    port

--- src/main/java/ru/rsreu/port/enums/Jsp.java
+++ src/main/java/ru/rsreu/port/enums/Jsp.java
@@ -5,7 +5,7 @@ public enum Jsp {
     ADMIN_PROFILE("/jsp/admin/adminProfile.jsp"),
     DISPATCHER_PROFILE("/jsp/dispatcher/dispatcherProfile.jsp"),
     CAPTAIN_PROFILE("/jsp/captain/captainProfile.jsp"),
-    CREATE_REQUEST("jsp/captain/createRequest.jsp"),
+    CREATE_REQUEST("/jsp/captain/createRequest.jsp"),
     NOT_FOUND("/jsp/404.jsp");
     private final String route;
 
